e00d606f49a37959974f21f51a3ab989ce4db892
Tushant Yadav <56768158+tushant-yadav@users.noreply.github.com>
2024-08-04 19:00:27 +0530

    Counting setbit question for Bit Manipulation

---
+++ Number of 1 Bits.cpp
@@ -0,0 +1,67 @@
+/*
+umber of 1 Bits
+Difficulty: EasyAccuracy: 76.5%Submissions: 136K+Points: 2
+Given a positive integer N, print count of set bits in it. 
+
+Example 1:
+
+Input:
+N = 6
+Output:
+2
+Explanation:
+Binary representation is '110' 
+So the count of the set bit is 2.
+Example 2:
+
+Input:
+8
+Output:
+1
+Explanation:
+Binary representation is '1000' 
+So the count of the set bit is 1.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function setBits() which takes an Integer N and returns the count of number of set bits.
+
+Expected Time Complexity: O(LogN)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 109
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        int cnt = 0;
+        while(N>0){
+            cnt+=(N&1);
+            N=N>>1;
+        }
+        return cnt;
+        // Write Your Code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
